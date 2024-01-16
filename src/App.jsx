b9e4800809835398500a10021202e76012fcf9ab
Jimmy Givans <jauyoma.dev@gmail.com>
2024-01-16 17:07:33 +0300

    feat: add Education component
    - This component represents the Education section in our CV app.

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,37 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+export const Education = () => {
+  return (
+    <form action="/" method="post">
+      <div>
+        <label htmlFor="education">Education</label>
+        <input type="text" name="education" id="education" />
+      </div>
+
+      <div>
+        <label htmlFor="school">School</label>
+        <input type="text" name="school" id="school" />
+      </div>
+
+      <div>
+        <label htmlFor="start-date">Start Date</label>
+        <input type="date" name="start-date" id="start-date" />
+      </div>
+
+      <div>
+        <label htmlFor="end-date">End Date</label>
+        <input type="date" name="end-date" id="end-date" />
+      </div>
+
+      <div>
+        <label htmlFor="description">Description</label>
+        <textarea name="description" id="description" cols="30" rows="10"></textarea>
+      </div>
+    </form>
+  );
+};
+
 function App() {
   const [count, setCount] = useState(0)
 
