e59678fc87239fa3e8d89493e005be604e469911
Jimmy Givans <jauyoma.dev@gmail.com>
2024-01-23 16:21:20 +0300

    refactor: use the HistoryForm to render education history
    - Delete unused EducationForm component
    - Use imported HistoryForm component with formName = "Education"

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,10 @@
 import { TextArea } from "./components/TextArea";
-
-export const EducationForm = () => {
-  return (
-    <form action="/" method="post">
-      <div>
-        <label htmlFor="education">Education</label>
-        <input type="text" name="education" id="education" />
-      </div>
-
-      <div>
-        <label htmlFor="school">School</label>
-        <input type="text" name="school" id="school" />
-      </div>
-
-      <fieldset className="dates">
-        <div>
-          <label htmlFor="start-date">Start Date</label>
-          <input type="date" name="start-date" id="start-date" />
-        </div>
-
-        <div>
-          <label htmlFor="end-date">End Date</label>
-          <input type="date" name="end-date" id="end-date" />
-        </div>
-      </fieldset>
-
-      <div>
-        <label htmlFor="description">Description</label>
-        <textarea name="description" id="description" cols="30" rows="10"></textarea>
-      </div>
-    </form>
-  );
-};
+import { HistoryForm } from "./components/HistoryForm";
 
 function App() {
   return (
     <main>
+      <HistoryForm formName={"Education"} />
       <TextArea sectionName={"Qualifications"} />
       <TextArea sectionName={"Interests"} />
       <TextArea sectionName={"References"} />
