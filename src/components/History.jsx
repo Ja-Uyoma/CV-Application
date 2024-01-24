152d6a801d433ee7ff8258cfd24fe1bc013a19c0
Jimmy Givans <jauyoma.dev@gmail.com>
2024-01-24 16:19:13 +0300

    feat: style the History component

--- src/components/History.jsx
+++ src/components/History.jsx
@@ -9,34 +9,55 @@ import PropTypes from "prop-types";
 const History = ({ formName }) => {
   return (
     <section>
-      <h2>{formName}</h2>
+      <h2 className="font-bold text-xl py-4">{formName}</h2>
 
-      <form action="/" method="post" autoComplete="off">
-        <div>
-          <label htmlFor="education">{formName === "Education" ? "Education" : "Position"}</label>
-          <input type="text" name="education" id="education" />
+      <form action="/" method="post" autoComplete="off" className="border border-solid rounded-lg border-gray-900 p-4">
+        <div className="my-2">
+          <label htmlFor="education" className="block font-medium text-gray-700">
+            {formName === "Education" ? "Education" : "Position"}
+          </label>
+          <input type="text" name="education" id="education" className="bg-gray-100 rounded-lg border-none w-full" />
         </div>
 
-        <div>
-          <label htmlFor="school">{formName === "Education" ? "School" : "Employer"}</label>
-          <input type="text" name="school" id="school" />
+        <div className="my-2">
+          <label htmlFor="school" className="block font-medium text-gray-700">
+            {formName === "Education" ? "School" : "Employer"}
+          </label>
+          <input type="text" name="school" id="school" className="bg-gray-100 rounded-lg border-none w-full" />
         </div>
 
-        <div>
-          <div>
-            <label htmlFor="start-date">Start Date</label>
-            <input type="date" name="start-date" id="start-date" />
+        <div className="flex gap-2 my-2">
+          <div className="flex-1">
+            <label htmlFor="start-date" className="block font-medium text-gray-700">
+              Start Date
+            </label>
+            <input
+              type="date"
+              name="start-date"
+              id="start-date"
+              className="bg-gray-100 rounded-lg border-none w-full"
+            />
           </div>
 
-          <div>
-            <label htmlFor="end-date">End Date</label>
-            <input type="date" name="end-date" id="end-date" />
+          <div className="flex-1 my-2">
+            <label htmlFor="end-date" className="block font-medium text-gray-700">
+              End Date
+            </label>
+            <input type="date" name="end-date" id="end-date" className="bg-gray-100 rounded-lg border-none w-full" />
           </div>
         </div>
 
-        <div>
-          <label htmlFor="description">Description</label>
-          <textarea name="description" id="description" cols="30" rows="10"></textarea>
+        <div className="my-2">
+          <label htmlFor="description" className="block font-medium text-gray-700">
+            Description
+          </label>
+          <textarea
+            name="description"
+            id="description"
+            cols="30"
+            rows="10"
+            className="bg-gray-100 border-none w-full"
+          ></textarea>
         </div>
       </form>
     </section>
