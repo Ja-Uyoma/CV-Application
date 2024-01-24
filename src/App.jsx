826da198d086fe79bee66be43f81fade35159ea7
Jimmy Givans <jauyoma.dev@gmail.com>
2024-01-24 16:57:16 +0300

    refactor: move other components into ResumeBuilder component

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,9 @@
-import { TextArea } from "./components/TextArea";
-import { History } from "./components/History";
-import { Certificates } from "./components/Certificates";
-import { BasicInformation } from "./components/BasicInformation";
+import { ResumeBuilder } from "./components/ResumeBuilder";
 
 function App() {
   return (
     <main>
-      <BasicInformation />
-      <History formName={"Education"} />
-      <TextArea sectionName={"Qualifications"} />
-      <TextArea sectionName={"Interests"} />
-      <TextArea sectionName={"References"} />
-      <TextArea sectionName={"Skills"} />
-      <History formName={"Work Experience"} />
-      <Certificates />
+      <ResumeBuilder />
     </main>
   );
 }
