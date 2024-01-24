a5169e1e27a6b77705fef2d0419430d06aaa1cdc
Jimmy Givans <jauyoma.dev@gmail.com>
2024-01-24 09:21:17 +0300

    feat: style the TextArea component
    - Make the section name text bold, extra large, and add a top and
    bottom margin of 1.25rem
    - Make the label description text medium font, and colour gray

--- src/components/TextArea.jsx
+++ src/components/TextArea.jsx
@@ -3,13 +3,13 @@ import PropTypes from "prop-types";
 const TextArea = ({ sectionName }) => {
   return (
     <section>
-      <h2 className="font-bold text-xl">{sectionName}</h2>
+      <h2 className="font-bold text-xl my-5">{sectionName}</h2>
 
       <form action="/" method="post" autoComplete="off">
         <label htmlFor="description">
-          <div className="block">Description</div>
+          <div className="font-medium text-gray-700">Description</div>
           {/** */}
-          <textarea name="description" id="description" cols="30" rows="10"></textarea>
+          <textarea name="description" id="description" cols="80" rows="10" className="bg-gray-100"></textarea>
         </label>
       </form>
     </section>
