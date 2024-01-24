9e01aa7103cfbc6c3101eecbcb8bf15c74e30164
Jimmy Givans <jauyoma.dev@gmail.com>
2024-01-24 16:28:47 +0300

    feat: style the input fields

--- src/components/BasicInformation.jsx
+++ src/components/BasicInformation.jsx
@@ -9,7 +9,12 @@ const BasicInformation = () => {
             <label htmlFor="given-name">
               <span className="block font-medium text-gray-700">Given Name</span>
               {/** */}
-              <input type="text" name="given-name" id="given-name" />
+              <input
+                type="text"
+                name="given-name"
+                id="given-name"
+                className="bg-gray-100 rounded-lg border-none w-full"
+              />
             </label>
           </div>
 
@@ -17,7 +22,12 @@ const BasicInformation = () => {
             <label htmlFor="family-name">
               <span className="block font-medium text-gray-700">Family Name</span>
               {/** */}
-              <input type="text" name="family-name" id="family-name" />
+              <input
+                type="text"
+                name="family-name"
+                id="family-name"
+                className="bg-gray-100 rounded-lg border-none w-full"
+              />
             </label>
           </div>
 
@@ -25,7 +35,7 @@ const BasicInformation = () => {
             <label htmlFor="email">
               <span className="block font-medium text-gray-700">Email Address</span>
               {/** */}
-              <input type="email" name="email" id="email" />
+              <input type="email" name="email" id="email" className="bg-gray-100 rounded-lg border-none w-full" />
             </label>
           </div>
         </div>
@@ -34,7 +44,7 @@ const BasicInformation = () => {
           <label htmlFor="headline">
             <span className="block font-medium text-gray-700">Headline</span>
             {/** */}
-            <input type="text" name="headline" id="headline" />
+            <input type="text" name="headline" id="headline" className="bg-gray-100 rounded-lg border-none w-full" />
           </label>
         </div>
 
@@ -42,7 +52,12 @@ const BasicInformation = () => {
           <label htmlFor="phone-number">
             <span className="block font-medium text-gray-700">Phone Number</span>
             {/** */}
-            <input type="tel" name="phone-number" id="phone-number" />
+            <input
+              type="tel"
+              name="phone-number"
+              id="phone-number"
+              className="bg-gray-100 rounded-lg border-none w-full"
+            />
           </label>
         </div>
 
@@ -50,7 +65,7 @@ const BasicInformation = () => {
           <label htmlFor="address">
             <span className="block font-medium text-gray-700">Address</span>
             {/** */}
-            <input type="text" name="address" id="address" />
+            <input type="text" name="address" id="address" className="bg-gray-100 rounded-lg border-none w-full" />
           </label>
         </div>
 
@@ -59,7 +74,12 @@ const BasicInformation = () => {
             <label htmlFor="post-code">
               <span className="block font-medium text-gray-700">Postal code</span>
               {/** */}
-              <input type="text" name="post-code" id="post-code" />
+              <input
+                type="text"
+                name="post-code"
+                id="post-code"
+                className="bg-gray-100 rounded-lg border-none w-full"
+              />
             </label>
           </div>
 
@@ -67,7 +87,7 @@ const BasicInformation = () => {
             <label htmlFor="city">
               <span className="block font-medium text-gray-700">City</span>
               {/** */}
-              <input type="text" name="city" id="city" />
+              <input type="text" name="city" id="city" className="bg-gray-100 rounded-lg border-none w-full" />
             </label>
           </div>
         </div>
