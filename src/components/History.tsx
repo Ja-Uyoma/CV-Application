4b6a49272383cfdf0c02b9b1b31726384a8f647c
Jimmy Givans <jauyoma.dev@gmail.com>
2024-10-02 13:31:40 +0300

    style: format document using Prettier

--- src/components/History.tsx
+++ src/components/History.tsx
@@ -11,24 +11,45 @@ const History = ({ formName }) => {
     <section>
       <h2 className="font-bold text-xl py-4">{formName}</h2>
 
-      <form action="/" method="post" autoComplete="off" className="border border-solid rounded-lg border-gray-900 p-4">
+      <form
+        action="/"
+        method="post"
+        autoComplete="off"
+        className="border border-solid rounded-lg border-gray-900 p-4"
+      >
         <div className="my-2">
-          <label htmlFor="education" className="block font-medium text-gray-700">
+          <label
+            htmlFor="education"
+            className="block font-medium text-gray-700"
+          >
             {formName === "Education" ? "Education" : "Position"}
           </label>
-          <input type="text" name="education" id="education" className="bg-gray-100 rounded-lg border-none w-full" />
+          <input
+            type="text"
+            name="education"
+            id="education"
+            className="bg-gray-100 rounded-lg border-none w-full"
+          />
         </div>
 
         <div className="my-2">
           <label htmlFor="school" className="block font-medium text-gray-700">
             {formName === "Education" ? "School" : "Employer"}
           </label>
-          <input type="text" name="school" id="school" className="bg-gray-100 rounded-lg border-none w-full" />
+          <input
+            type="text"
+            name="school"
+            id="school"
+            className="bg-gray-100 rounded-lg border-none w-full"
+          />
         </div>
 
         <div className="flex gap-2 my-2">
           <div className="flex-1">
-            <label htmlFor="start-date" className="block font-medium text-gray-700">
+            <label
+              htmlFor="start-date"
+              className="block font-medium text-gray-700"
+            >
               Start Date
             </label>
             <input
@@ -40,15 +61,26 @@ const History = ({ formName }) => {
           </div>
 
           <div className="flex-1">
-            <label htmlFor="end-date" className="block font-medium text-gray-700">
+            <label
+              htmlFor="end-date"
+              className="block font-medium text-gray-700"
+            >
               End Date
             </label>
-            <input type="date" name="end-date" id="end-date" className="bg-gray-100 rounded-lg border-none w-full" />
+            <input
+              type="date"
+              name="end-date"
+              id="end-date"
+              className="bg-gray-100 rounded-lg border-none w-full"
+            />
           </div>
         </div>
 
         <div className="my-2">
-          <label htmlFor="description" className="block font-medium text-gray-700">
+          <label
+            htmlFor="description"
+            className="block font-medium text-gray-700"
+          >
             Description
           </label>
           <textarea
