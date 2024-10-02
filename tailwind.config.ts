df64dc7f36091dac9f78ff6a5095cecf186dd26a
Jimmy Givans <jauyoma.dev@gmail.com>
2024-10-02 15:53:56 +0300

    build: use ESM import syntax when adding plugins

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,11 @@
 import type { Config } from "tailwindcss";
+import tailwindcssforms from "@tailwindcss/forms";
+import daisyui from "daisyui";
 
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [require("@tailwindcss/forms"), require("daisyui")],
+  plugins: [tailwindcssforms, daisyui],
 } satisfies Config;
