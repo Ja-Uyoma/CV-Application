fd7d42fc1549a27cda440efc35cf0a17a27dce8e
Jimmy Givans <jauyoma.dev@gmail.com>
2024-10-02 16:24:25 +0300

    refactor: parse name into a valid id before assigning it to the for and id attributes

--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -1,12 +1,14 @@
+import { createIDFromName } from "../main";
+
 export const TextInput = ({ name }: { name: string }) => {
   return (
     <>
-      <label htmlFor={name}>
+      <label htmlFor={createIDFromName(name)}>
         <span className="block font-medium text-gray-700">{name}</span>
         <input
           type="text"
           name={name}
-          id={name}
+          id={createIDFromName(name)}
           className="bg-gray-100 rounded-lg border-none w-full"
         />
       </label>
