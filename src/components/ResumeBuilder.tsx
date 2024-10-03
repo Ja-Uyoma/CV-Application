05f177c9ad38d5c442e6d860af7e71addb80b828
Jimmy Givans <jauyoma.dev@gmail.com>
2024-10-03 13:36:23 +0300

    refactor: use Education component to render Education history

--- src/components/ResumeBuilder.tsx
+++ src/components/ResumeBuilder.tsx
@@ -2,12 +2,13 @@ import { Pane } from "./Pane.tsx";
 import { History } from "./History.tsx";
 import { Certificates } from "./Certificates.tsx";
 import { BasicInformation } from "./BasicInformation.tsx";
+import { Education } from "./Education.tsx";
 
 export const ResumeBuilder = () => {
   return (
     <div className="w-1/2 m-auto">
       <BasicInformation />
-      <History formName={"Education"} />
+      <Education />
       <Pane sectionName={"Qualifications"} />
       <Pane sectionName={"Interests"} />
       <Pane sectionName={"References"} />
