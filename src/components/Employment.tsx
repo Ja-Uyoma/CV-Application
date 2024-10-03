edce0ca6ccc9bedfb0314fada75e8a20e98fd095
Jimmy Givans <jauyoma.dev@gmail.com>
2024-10-03 15:41:26 +0300

    refactor: delete unnecessary fragment

--- src/components/Employment.tsx
+++ src/components/Employment.tsx
@@ -4,28 +4,26 @@ import { TextField } from "./TextField";
 
 export const Employment = () => {
   return (
-    <>
-      <section>
-        <h2 className="font-bold text-xl py-4">Employment</h2>
+    <section>
+      <h2 className="font-bold text-xl py-4">Employment</h2>
 
-        <form
-          action="/"
-          method="post"
-          className="border border-solid rounded-lg border-gray-900 p-4"
-        >
-          <>
-            <TextField name="Position" />
-            <TextField name="Employer" />
-          </>
-          <div className="flex gap-2">
-            <Date name="Start Date" />
-            <Date name="End Date" />
-          </div>
-          <>
-            <TextArea name="Description" />
-          </>
-        </form>
-      </section>
-    </>
+      <form
+        action="/"
+        method="post"
+        className="border border-solid rounded-lg border-gray-900 p-4"
+      >
+        <>
+          <TextField name="Position" />
+          <TextField name="Employer" />
+        </>
+        <div className="flex gap-2">
+          <Date name="Start Date" />
+          <Date name="End Date" />
+        </div>
+        <>
+          <TextArea name="Description" />
+        </>
+      </form>
+    </section>
   );
 };
