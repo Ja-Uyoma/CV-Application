78aba6066a928942396e5d763518a6fe7a249493
Jimmy Givans <jauyoma.dev@gmail.com>
2024-10-03 15:21:55 +0300

    refactor: delete ChevronUp and PlusCircle components

--- src/components/BasicInformation.tsx
+++ src/components/BasicInformation.tsx
@@ -4,46 +4,6 @@ import { TelephoneInput } from "./TelephoneInput";
 import { TextField } from "./TextField";
 import { CircleButton } from "./Buttons";
 
-const ChevronUp = () => {
-  return (
-    <>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        strokeWidth={1.5}
-        stroke="currentColor"
-        className="size-6"
-      >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          d="m4.5 15.75 7.5-7.5 7.5 7.5"
-        />
-      </svg>
-    </>
-  );
-};
-
-const PlusCircle = () => {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
-      strokeWidth={1.5}
-      stroke="currentColor"
-      className="size-6"
-    >
-      <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
-      />
-    </svg>
-  );
-};
-
 const BasicInformation = () => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
   const sectionRef = useRef<HTMLElement | null>(null);
