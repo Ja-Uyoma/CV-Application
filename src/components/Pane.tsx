ff65539dd9a1892c9974158eeaaf48ddf9a19de4
Jimmy Givans <jauyoma.dev@gmail.com>
2024-10-03 15:34:51 +0300

    refactor: make Pane component collapsible

--- src/components/Pane.tsx
+++ src/components/Pane.tsx
@@ -1,12 +1,45 @@
 import PropTypes from "prop-types";
 import { TextArea } from "./TextArea";
+import { useEffect, useRef, useState } from "react";
+import { CircleButton } from "./Buttons";
 
 const Pane = ({ sectionName }: { sectionName: string }) => {
-  return (
-    <section className="my-4">
-      <h2 className="font-bold text-xl my-5">{sectionName}</h2>
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const sectionRef = useRef<HTMLElement | null>(null);
 
-      <form action="/" method="post" autoComplete="off">
+  const onButtonClicked = () => {
+    setIsOpen(!isOpen);
+  };
+
+  useEffect(() => {
+    const section = sectionRef.current;
+
+    if (section && isOpen) {
+      section.classList.remove("collapse-close");
+      section.classList.add("collapse-open");
+    } else if (section && !isOpen) {
+      section.classList.remove("collapse-open");
+      section.classList.add("collapse-close");
+    }
+  }, [isOpen]);
+
+  return (
+    <section
+      tabIndex={0}
+      className="my-4 collapse collapse-close"
+      ref={sectionRef}
+    >
+      <div className="collapse-title flex justify-between items-center">
+        <h2 className="font-bold text-xl py-4">{sectionName}</h2>
+        <CircleButton handleClick={() => onButtonClicked()} />
+      </div>
+
+      <form
+        action="/"
+        method="post"
+        autoComplete="off"
+        className="collapse-content"
+      >
         <TextArea name="Description" />
       </form>
     </section>
