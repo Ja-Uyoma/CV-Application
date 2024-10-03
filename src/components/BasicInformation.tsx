2c1b984eaec34363feb5fe4962fd1727ccd0e446
Jimmy Givans <jauyoma.dev@gmail.com>
2024-10-03 14:38:13 +0300

    feat: add ChevronUp component

--- src/components/BasicInformation.tsx
+++ src/components/BasicInformation.tsx
@@ -2,6 +2,27 @@ import { EmailInput } from "./EmailInput";
 import { TelephoneInput } from "./TelephoneInput";
 import { TextField } from "./TextField";
 
+const ChevronUp = () => {
+  return (
+    <>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={1.5}
+        stroke="currentColor"
+        className="size-6"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="m4.5 15.75 7.5-7.5 7.5 7.5"
+        />
+      </svg>
+    </>
+  );
+};
+
 const BasicInformation = () => {
   return (
     <section tabIndex={0} className="collapse collapse-open">
