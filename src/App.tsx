35be056a8a636beaab22d2a6eca16de6a3d264df
Jimmy Givans <jauyoma.dev@gmail.com>
2024-10-08 14:49:12 +0300

    refactor: embed contents of ResumeBuilder component in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
-import { ResumeBuilder } from "./components/ResumeBuilder";
+import { Certificates } from "./components/Certificates";
+import { Education } from "./components/Education";
+import { Employment } from "./components/Employment";
+import { Pane } from "./components/Pane";
+import { PersonalDetails } from "./components/PersonalDetails";
 
 function App() {
   return (
     <main className="bg-primary flex">
-      <ResumeBuilder />
+      <div className="w-1/2 flex flex-col gap-2">
+        <PersonalDetails />
+        <Education />
+        <Pane name={"Qualifications"} />
+        <Pane name={"Interests"} />
+        <Pane name={"References"} />
+        <Pane name={"Skills"} />
+        <Employment />
+        <Certificates />
+      </div>
 
       <div>
         <p>View Your Resume!</p>
