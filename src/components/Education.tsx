295dd8c04e52292b8d12bc11b423c86717404578
Jimmy Givans <jauyoma.dev@gmail.com>
2024-10-28 13:55:23 +0300

    feat: create a store for Education component data

--- src/components/Education.tsx
+++ src/components/Education.tsx
@@ -1,5 +1,27 @@
 import { useForm } from "react-hook-form";
 import { EducationData } from "../types/Education";
+import { create } from "zustand";
+
+type Action = {
+  updateEducation: (education: EducationData["education"]) => void;
+  updateSchool: (school: EducationData["school"]) => void;
+  updateStartDate: (startDate: EducationData["startDate"]) => void;
+  updateEndDate: (endDate: EducationData["endDate"]) => void;
+  updateDescription: (description: EducationData["description"]) => void;
+};
+
+const useEducationStore = create<EducationData & Action>((set) => ({
+  education: "",
+  school: "",
+  startDate: "",
+  endDate: "",
+  description: "",
+  updateEducation: (education) => set(() => ({ education: education })),
+  updateSchool: (school) => set(() => ({ school: school })),
+  updateStartDate: (startDate) => set(() => ({ startDate: startDate })),
+  updateEndDate: (endDate) => set(() => ({ endDate: endDate })),
+  updateDescription: (description) => set(() => ({ description: description })),
+}));
 
 export function Education() {
   const { register } = useForm<EducationData>();
