cd5b5c59b694fe066d7db6cec6e41e699df05e6d
Jimmy Givans <jauyoma.dev@gmail.com>
2024-10-28 14:41:06 +0300

    feat: create an EducationPreview component

--- src/components/Education.tsx
+++ src/components/Education.tsx
@@ -123,3 +123,21 @@ export function Education() {
     </details>
   );
 }
+
+export function EducationPreview() {
+  const education = useEducationStore((state) => state.education);
+  const school = useEducationStore((state) => state.school);
+  const startDate = useEducationStore((state) => state.startDate);
+  const endDate = useEducationStore((state) => state.endDate);
+  const description = useEducationStore((state) => state.description);
+
+  return (
+    <>
+      <p>{education}</p>
+      <p>{school}</p>
+      <p>{startDate}</p>
+      <p>{endDate}</p>
+      <p>{description}</p>
+    </>
+  );
+}
