1f89f3a4c6bc881ef46a5bfa854200d363746532
Jimmy Givans <jauyoma.dev@gmail.com>
2024-10-28 15:37:16 +0300

    feat: create a store for Employment data

--- src/components/Employment.tsx
+++ src/components/Employment.tsx
@@ -1,5 +1,35 @@
 import { useForm } from "react-hook-form";
 import { EmploymentData } from "../types/Employment";
+import { create } from "zustand";
+
+type State = {
+  position: string;
+  employer: string;
+  startDate: string;
+  endDate: string;
+  description: string;
+};
+
+type Action = {
+  updatePosition: (position: State["position"]) => void;
+  updateEmployer: (employer: State["employer"]) => void;
+  updateStartDate: (sdate: State["startDate"]) => void;
+  updateEndDate: (edate: State["endDate"]) => void;
+  updateDescription: (desc: State["description"]) => void;
+};
+
+const useStore = create<State & Action>((set) => ({
+  position: "",
+  employer: "",
+  startDate: "",
+  endDate: "",
+  description: "",
+  updatePosition: (pos) => set(() => ({ position: pos })),
+  updateEmployer: (employer) => set(() => ({ employer: employer })),
+  updateStartDate: (sdate) => set(() => ({ startDate: sdate })),
+  updateEndDate: (edate) => set(() => ({ endDate: edate })),
+  updateDescription: (desc) => set(() => ({ description: desc })),
+}));
 
 export function Employment() {
   const { register } = useForm<EmploymentData>();
