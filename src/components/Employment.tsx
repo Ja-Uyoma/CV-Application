36bc4d67b7dc439a6767cbcb9087941b29ac1c98
Jimmy Givans <jauyoma.dev@gmail.com>
2024-10-28 15:47:54 +0300

    feat: create a preview component for Employment data

--- src/components/Employment.tsx
+++ src/components/Employment.tsx
@@ -131,3 +131,21 @@ export function Employment() {
     </details>
   );
 }
+
+export function EmploymentPreview() {
+  const position = useStore((state) => state.position);
+  const employer = useStore((state) => state.employer);
+  const startDate = useStore((state) => state.startDate);
+  const endDate = useStore((state) => state.endDate);
+  const description = useStore((state) => state.description);
+
+  return (
+    <>
+      {position}
+      {employer}
+      {startDate}
+      {endDate}
+      {description}
+    </>
+  );
+}
