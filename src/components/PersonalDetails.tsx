6b37ca65d8c21990febb6ae91a23833620a488c1
Jimmy Givans <jauyoma.dev@gmail.com>
2024-10-28 16:00:00 +0300

    feat: create a store for the PersonalDetails component

--- src/components/PersonalDetails.tsx
+++ src/components/PersonalDetails.tsx
@@ -1,6 +1,48 @@
 import { useForm } from "react-hook-form";
 import { PersonalDetailsData } from "../types/PersonalDetails";
 import { useEffect } from "react";
+import { create } from "zustand";
+
+type State = {
+  givenName: string;
+  familyName: string;
+  headline: string;
+  emailAddress: string;
+  phoneNumber: string;
+  address: string;
+  postCode: string;
+  city: string;
+};
+
+type Action = {
+  updateGivenName: (givenName: State["givenName"]) => void;
+  updateFamilyName: (familyName: State["familyName"]) => void;
+  updateHeadline: (headline: State["headline"]) => void;
+  updateEmail: (email: State["emailAddress"]) => void;
+  updatePhoneNumber: (phoneNumber: State["phoneNumber"]) => void;
+  updateAddress: (addr: State["address"]) => void;
+  updatePostCode: (postCode: State["postCode"]) => void;
+  updateCity: (city: State["city"]) => void;
+};
+
+const useStore = create<State & Action>((set) => ({
+  givenName: "",
+  familyName: "",
+  headline: "",
+  emailAddress: "",
+  phoneNumber: "",
+  address: "",
+  postCode: "",
+  city: "",
+  updateGivenName: (givenName) => set(() => ({ givenName: givenName })),
+  updateFamilyName: (familyName) => set(() => ({ familyName: familyName })),
+  updateHeadline: (headline) => set(() => ({ headline: headline })),
+  updateEmail: (email) => set(() => ({ emailAddress: email })),
+  updatePhoneNumber: (phoneNumber) => set(() => ({ phoneNumber: phoneNumber })),
+  updateAddress: (addr) => set(() => ({ address: addr })),
+  updatePostCode: (postCode) => set(() => ({ postCode: postCode })),
+  updateCity: (city) => set(() => ({ city: city })),
+}));
 
 function PersonalDetails(props: {
   setDetails: (details: PersonalDetailsData) => void;
