d67b65c563414b205af31b35311ed785cf12a34c
Jimmy Givans <jauyoma.dev@gmail.com>
2024-10-29 16:03:10 +0300

    refactor: move Preview component to src/Preview.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,4 @@
-import { CertificatesPreview } from "./components/Certificates";
-import { EducationPreview } from "./components/Education";
-import { EmploymentPreview } from "./components/Employment";
-import { InterestsPreview } from "./components/Interests";
-import { PersonalDetailsPreview } from "./components/PersonalDetails";
-import { QualificationsPreview } from "./components/Qualifications";
-import { ReferencesPreview } from "./components/References";
-import { SkillsPreview } from "./components/Skills";
+import { Preview } from "./Preview";
 import { Resume } from "./Resume";
 
 function App() {
@@ -14,22 +7,6 @@ function App() {
       <Resume />
       <Preview />
     </main>
-  );
-}
-
-function Preview() {
-  return (
-    <div className="flex-1">
-      <p className="text-center font-medium text-xl">Resume</p>
-      <PersonalDetailsPreview />
-      <EducationPreview />
-      <QualificationsPreview />
-      <InterestsPreview />
-      <ReferencesPreview />
-      <SkillsPreview />
-      <EmploymentPreview />
-      <CertificatesPreview />
-    </div>
   );
 }
 
---
+++ src/Preview.tsx
@@ -0,0 +1,24 @@
+import { CertificatesPreview } from "./components/Certificates";
+import { EducationPreview } from "./components/Education";
+import { EmploymentPreview } from "./components/Employment";
+import { InterestsPreview } from "./components/Interests";
+import { PersonalDetailsPreview } from "./components/PersonalDetails";
+import { QualificationsPreview } from "./components/Qualifications";
+import { ReferencesPreview } from "./components/References";
+import { SkillsPreview } from "./components/Skills";
+
+export function Preview() {
+  return (
+    <div className="flex-1">
+      <p className="text-center font-medium text-xl">Resume</p>
+      <PersonalDetailsPreview />
+      <EducationPreview />
+      <QualificationsPreview />
+      <InterestsPreview />
+      <ReferencesPreview />
+      <SkillsPreview />
+      <EmploymentPreview />
+      <CertificatesPreview />
+    </div>
+  );
+}
