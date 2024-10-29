e74db02ceb54ebdce458c89c4b9c38e0a3d36f15
Jimmy Givans <jauyoma.dev@gmail.com>
2024-10-29 15:59:05 +0300

    refactor: change size of padding

--- src/components/Details.tsx
+++ src/components/Details.tsx
@@ -2,7 +2,7 @@ export function Details(props: { name: string; children: React.ReactNode }) {
   return (
     <details
       name={props.name}
-      className="rounded-none space-y-2 cursor-pointer py-3 w-11/12 mx-auto"
+      className="rounded-none space-y-2 cursor-pointer py-2 w-11/12 mx-auto"
     >
       {props.children}
     </details>
