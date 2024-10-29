25fbfcef40e0ac8f0ffb2fd50f2d3beb3d178108
Jimmy Givans <jauyoma.dev@gmail.com>
2024-10-29 18:34:49 +0300

    refactor: move types from hook.ts and export them with aliases

---
+++ src/components/employment/types.ts
@@ -0,0 +1,17 @@
+type State = {
+  position: string;
+  employer: string;
+  startDate: string;
+  endDate: string;
+  description: string;
+};
+
+type Action = {
+  updatePosition: (position: State["position"]) => void;
+  updateEmployer: (employer: State["employer"]) => void;
+  updateStartDate: (sdate: State["startDate"]) => void;
+  updateEndDate: (edate: State["endDate"]) => void;
+  updateDescription: (desc: State["description"]) => void;
+};
+
+export { type State as EmploymentState, type Action as EmploymentAction };
