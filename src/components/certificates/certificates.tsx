92a10a0411b1109a70b3ee2c6dfa089d7c77eefc
Jimmy Givans <jauyoma.dev@gmail.com>
2024-10-29 17:51:20 +0300

    refactor: move months.tsx to months/months.tsx
    - Move src/components/certificates/months.tsx to
      src/components/certificates/months/months.tsx
    - Update the import paths to this file in certificates.tsx and
      certificates-preview.tsx

--- src/components/certificates/certificates.tsx
+++ src/components/certificates/certificates.tsx
@@ -1,5 +1,5 @@
 import { ChangeEvent, useState } from "react";
-import { Months } from "./months";
+import { Months } from "./months/months";
 import { Years } from "./years";
 import { CheckBox } from "./check-box";
 import { useForm } from "react-hook-form";
