1660ff22683904bcbca89d3173d3a778d4d747e8
Jimmy Givans <jauyoma.dev@gmail.com>
2024-10-29 16:00:52 +0300

    refactor: move Resume component to src/Resume.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,12 @@
-import { Certificates, CertificatesPreview } from "./components/Certificates";
-import { Education, EducationPreview } from "./components/Education";
-import { Employment, EmploymentPreview } from "./components/Employment";
-import { Interests, InterestsPreview } from "./components/Interests";
-import {
-  PersonalDetails,
-  PersonalDetailsPreview,
-} from "./components/PersonalDetails";
-import {
-  Qualifications,
-  QualificationsPreview,
-} from "./components/Qualifications";
-import { References, ReferencesPreview } from "./components/References";
-import { Skills, SkillsPreview } from "./components/Skills";
+import { CertificatesPreview } from "./components/Certificates";
+import { EducationPreview } from "./components/Education";
+import { EmploymentPreview } from "./components/Employment";
+import { InterestsPreview } from "./components/Interests";
+import { PersonalDetailsPreview } from "./components/PersonalDetails";
+import { QualificationsPreview } from "./components/Qualifications";
+import { ReferencesPreview } from "./components/References";
+import { SkillsPreview } from "./components/Skills";
+import { Resume } from "./Resume";
 
 function App() {
   return (
@@ -19,28 +14,6 @@ function App() {
       <Resume />
       <Preview />
     </main>
-  );
-}
-
-function Resume() {
-  return (
-    <div className="flex flex-1 flex-col gap-1 bg-white">
-      <PersonalDetails />
-      <hr className="w-11/12 mx-auto" />
-      <Education />
-      <hr className="w-11/12 mx-auto" />
-      <Qualifications />
-      <hr className="w-11/12 mx-auto" />
-      <Interests />
-      <hr className="w-11/12 mx-auto" />
-      <References />
-      <hr className="w-11/12 mx-auto" />
-      <Skills />
-      <hr className="w-11/12 mx-auto" />
-      <Employment />
-      <hr className="w-11/12 mx-auto" />
-      <Certificates />
-    </div>
   );
 }
 
