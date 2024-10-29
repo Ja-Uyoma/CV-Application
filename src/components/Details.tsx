a65d9a855443cec47af09361cde4cb33599381c6
Jimmy Givans <jauyoma.dev@gmail.com>
2024-10-29 15:51:52 +0300

    feat: add a details component

---
+++ src/components/Details.tsx
@@ -0,0 +1,10 @@
+export function Details(props: { name: string; children: React.ReactNode }) {
+  return (
+    <details
+      name={props.name}
+      className="rounded-none space-y-2 cursor-pointer pb-3 w-11/12 mx-auto"
+    >
+      {props.children}
+    </details>
+  );
+}
