770f4448d5bcaae59137a7be1a1a70a09494ad61
Jimmy Givans <jauyoma.dev@gmail.com>
2024-10-29 15:13:09 +0300

    feat: add a Summary component

---
+++ src/components/Summary.tsx
@@ -0,0 +1,10 @@
+export function Summary(props: { title: string }) {
+  return (
+    <summary className="list-none my-4 flex items-center justify-between">
+      <span className="inline-block font-bold text-xl">{props.title}</span>
+      <button className="inline-block">
+        <span className="material-symbols-outlined size-full">add_circle</span>
+      </button>
+    </summary>
+  );
+}
