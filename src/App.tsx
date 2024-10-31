a80d6fbc9f0126a799e1db073ec6a9a3c1d59016
Jimmy Givans <jauyoma.dev@gmail.com>
2024-10-31 18:12:44 +0300

    feat: enable the user to dynamically set the page title

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,36 @@
+import { ChangeEvent, useEffect, useState } from "react";
+
 import { Preview } from "./components/preview";
 import { Resume } from "./components/resume";
 
 function App() {
+  const [title, setTitle] = useState("Untitled resume");
+
+  useEffect(() => {
+    document.title = title;
+  }, [title]);
+
   return (
-    <main className="flex">
-      <Resume />
-      <Preview />
-    </main>
+    <>
+      <header className="text-center py-5 text-white font-medium bg-gray-900">
+        <h1>
+          <label>
+            <input
+              type="text"
+              placeholder="Untitled resume"
+              value={title}
+              onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                setTitle(e.target.value)
+              }
+            />
+          </label>
+        </h1>
+      </header>
+      <main className="flex">
+        <Resume />
+        <Preview />
+      </main>
+    </>
   );
 }
 
