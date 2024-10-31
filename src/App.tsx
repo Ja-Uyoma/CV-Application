d81b5b389250978fbed69a938f17b331e99f7dc4
Jimmy Givans <jauyoma.dev@gmail.com>
2024-10-31 19:06:43 +0300

    feat: set the width of the input field

--- src/App.tsx
+++ src/App.tsx
@@ -23,11 +23,12 @@ function App() {
               type="text"
               name="page-title"
               placeholder="Untitled resume"
-              className="bg-gray-900 p-0 border-0 size-max focus:ring-0 focus:border-b-2 focus:text-slate-400 focus:border-blue-300"
+              className="bg-gray-900 p-0 border-0 focus:ring-0 focus:border-b-2 focus:text-slate-400 focus:border-blue-300 max-w-40"
               value={title}
               onChange={(e: ChangeEvent<HTMLInputElement>) =>
                 setTitle(e.target.value)
               }
+              style={{ width: length * 8 }}
             />
           </label>
         </h1>
