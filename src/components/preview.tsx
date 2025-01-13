f83d107ed6aed91eec34e24b4418d53ac98aac50
Jimmy Givans <jauyoma.dev@gmail.com>
2025-01-13 17:35:52 +0300

    refactor: render user's name as resume title if given

--- src/components/preview.tsx
+++ src/components/preview.tsx
@@ -2,17 +2,28 @@ import { CertificatesPreview } from "./certificates/certificates-preview";
 import { EducationPreview } from "./education/education-preview";
 import { EmploymentPreview } from "./employment/employment-preview";
 import { InterestsPreview } from "./interests/interests-preview";
+import { usePersonalStore } from "./personal-details/hooks";
 import { PersonalDetailsPreview } from "./personal-details/personal-details-preview";
 import { QualificationsPreview } from "./qualifications/qualifications-preview";
 import { ReferencesPreview } from "./references/references-preview";
 import { SkillsPreview } from "./skills/skills-preview";
 
 export function Preview() {
+  const givenName = usePersonalStore((state) => state.givenName);
+  const familyName = usePersonalStore((state) => state.familyName);
+
   return (
     <div className="flex-1 bg-white">
-      <p className="text-center font-medium text-4xl uppercase text-black py-4">
-        Resume
-      </p>
+      {givenName || familyName ? (
+        <p className="text-center font-medium text-4xl uppercase text-black py-4">
+          {givenName} {familyName}
+        </p>
+      ) : (
+        <p className="text-center font-medium text-4xl uppercase text-black py-4">
+          Resume
+        </p>
+      )}
+
       <PersonalDetailsPreview />
       <EducationPreview />
       <QualificationsPreview />
