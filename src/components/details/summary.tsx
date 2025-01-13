386f8d1523cd27794cfcbf7fbc68d3e059cf92d7
Jimmy Givans <jauyoma.dev@gmail.com>
2025-01-13 12:37:00 +0300

    refactor: change cursor to a pointer when hovering above summary text

--- src/components/details/summary.tsx
+++ src/components/details/summary.tsx
@@ -1,7 +1,9 @@
 export function Summary(props: { title: string }) {
   return (
     <summary className="list-none my-4 flex items-center justify-between">
-      <span className="inline-block font-bold text-xl">{props.title}</span>
+      <span className="inline-block font-bold text-xl cursor-pointer">
+        {props.title}
+      </span>
       <button className="inline-block">
         <span className="material-symbols-outlined size-full">add_circle</span>
       </button>
