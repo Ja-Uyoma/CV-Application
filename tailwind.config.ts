c52ee0f1d62cc55462184190b5b569a9015e653f
Jimmy Givans <jauyoma.dev@gmail.com>
2025-05-05 16:22:03 +0300

    build: remove daisyui and plugins from my tailwind.config.ts
    - This is done so that the Tailwind CSS upgrade tool can safely replace
      it with a CSS file

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,10 @@
 import type { Config } from "tailwindcss";
 import tailwindcssforms from "@tailwindcss/forms";
-import daisyui from "daisyui";
 
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [tailwindcssforms, daisyui],
+  plugins: [tailwindcssforms],
 } satisfies Config;
