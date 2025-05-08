8339d35c9a0dd06313744b1eb5364bdaf2a40d25
Jimmy Givans <jauyoma.dev@gmail.com>
2025-05-08 15:49:45 +0300

    feat: add a border around the forms

--- src/components/form/form.tsx
+++ src/components/form/form.tsx
@@ -1,6 +1,10 @@
 export function Form(props: { children: React.ReactNode }) {
   return (
-    <form action="/" method="post" className="rounded-lg space-y-2">
+    <form
+      action="/"
+      method="post"
+      className="rounded-lg space-y-2 border-1 p-4 border-gray-400"
+    >
       {props.children}
     </form>
   );
